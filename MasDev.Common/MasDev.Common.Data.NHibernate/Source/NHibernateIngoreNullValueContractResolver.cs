eef3de61feaa0a8f025073eafe05c5023ece8f17
Gianluca Bonacin <gianlucabonacin@Gianlucas-MacBook-Pro.local>
2014-12-09 10:47:10 +0100

    contract serializer modularity

---
+++ MasDev.Common/MasDev.Common.Data.NHibernate/Source/NHibernateIngoreNullValueContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NHibernate.Proxy;
+using Newtonsoft.Json.Serialization;
+
+namespace MasDev.Common.Newtonsoft.ContractResolvers
+{
+	public class NHibernateContractResolver : IgnoreEmptyContractResolver
+	{
+		protected override JsonContract CreateContract (Type objectType)
+		{
+			if (typeof(NHibernate.Proxy.INHibernateProxy).IsAssignableFrom (objectType))
+				return base.CreateContract (objectType.BaseType);
+			else
+				return base.CreateContract (objectType);
+		}
+	}
+}
+
