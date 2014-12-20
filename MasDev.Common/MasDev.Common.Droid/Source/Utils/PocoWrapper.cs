4c392ae59f2e1d6cf598f1163167af8a0fad4274
francesco <francesco@francescos-MacBook-Pro.local>
2014-12-20 14:34:20 +0100

    New PagedEnumerable abstract class

---
+++ MasDev.Common/MasDev.Common.Droid/Source/Utils/PocoWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MasDev.Common.Droid
+{
+	public static class PocoWrapper 
+	{
+		public static PocoWrapper<T> Create<T> (Func<T, string> toString, T poco)
+		{
+			return new PocoWrapper<T> (toString, poco);
+		}
+
+		public static PocoWrapper<T> Create<T> (Func<T, string> toString)
+		{
+			return new PocoWrapper<T> (toString);
+		}
+	}
+
+	public class PocoWrapper<T> : Java.Lang.Object
+	{
+		public T Poco { get; set; }
+		readonly Func<T, string> _toString;
+
+		public PocoWrapper (Func<T, string> toString)
+		{
+			_toString = toString;
+		}
+
+		public PocoWrapper (Func<T, string> toString, T poco) : this (toString)
+		{
+			Poco = poco;
+		}
+
+		public override string ToString ()
+		{
+			return _toString.Invoke (Poco); //poco
+		}
+	}
+}
+
---
+++ MasDev.Common/MasDev.Common.Portable/Source/Collections/BasePagedEnumerable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MasDev.Common.Utils;
+
+namespace MasDev.Common.Collections
+{
+	public static class BasePagedEnumerable 
+	{
+		public static IPagedEnumerable<T> Create<T> (int pageSize, Func<int, int, Task<IEnumerable<T>>> retrivePageDelegate)
+		{
+			return new DelegateBasePagedEnumerable<T> (pageSize, retrivePageDelegate);
+		}
+	}
+
+	class DelegateBasePagedEnumerable<T> : BasePagedEnumerable<T>
+	{
+		readonly Func<int, int, Task<IEnumerable<T>>> _retrivePageDelegate;
+
+		public DelegateBasePagedEnumerable (int pageSize, Func<int, int, Task<IEnumerable<T>>> retrivePageDelegate) : base (pageSize)
+		{
+			_retrivePageDelegate = retrivePageDelegate;
+		}
+			
+		public override async Task<IEnumerable<T>> RetrivePageAsync (int currentPage)
+		{
+			return await _retrivePageDelegate.Invoke (currentPage, PageSize);
+		}
+	}
+
+	public abstract class BasePagedEnumerable<T> : IPagedEnumerable<T>
+	{
+		bool _finished;
+		public int PageSize { get; set; }
+		public int CurrentPage { get; set; }
+
+		public void Reset ()
+		{
+			CurrentPage = 0;
+			_finished = false;
+		}
+
+		protected BasePagedEnumerable (int pageSize)
+		{
+			CurrentPage = 0;
+			PageSize = pageSize;
+		}
+
+		public abstract Task<IEnumerable<T>> RetrivePageAsync (int currentPage);
+
+		public async Task<IEnumerable<T>> GetNextPageAsync ()
+		{
+			if (_finished)
+				throw new Exception ("No more pages");
+
+			try {
+				var page = await RetrivePageAsync (CurrentPage++);
+				_finished |= CollectionUtils.IsNullOrEmpty<T> (page);
+				return page;
+			} 
+			catch (Exception) {
+				CurrentPage--;
+				throw;
+			}
+		}
+
+		public bool HasMorePages { get { return !_finished; } }
+	}
+}
+
