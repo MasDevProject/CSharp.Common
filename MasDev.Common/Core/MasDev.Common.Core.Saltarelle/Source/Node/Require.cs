81f5f187233c9af5b1a718163e9219cfc38410cd
Gianluca Bonacin <gianlucabonacin@Gianlucas-MacBook-Pro.local>
2015-01-19 09:05:21 +0100

    typed require

---
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Node/Require.cs
@@ -0,0 +1,11 @@
+﻿namespace MasDev.Common.Saltarelle
+{
+	public static class Require
+	{
+		public static T Load<T> (string moduleName)
+		{
+			return NodeJS.Require.Load (moduleName);
+		}
+	}
+}
+
