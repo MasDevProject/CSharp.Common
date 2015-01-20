5f759abffa52c3a2619717da620147a77d011f63
Gianluca Bonacin <gianlucabonacin@Gianlucas-MacBook-Pro.local>
2015-01-20 11:19:13 +0100

    os utils

---
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Utils/OsUtils.cs
@@ -0,0 +1,13 @@
+﻿using NodeJS;
+
+namespace MasDev.Common.Saltarelle
+{
+	public static class OsUtils
+	{
+		public static bool IsWindows ()
+		{
+			return Process.Platform.ToLowerCase ().StartsWith ("win");
+		}
+	}
+}
+
