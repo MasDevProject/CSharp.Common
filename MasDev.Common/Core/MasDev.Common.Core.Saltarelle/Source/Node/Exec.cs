21318f7508fcd10baf9f4664a22c70ef243ccee8
Gianluca Bonacin <gianlucabonacin@Gianlucas-MacBook-Pro.local>
2015-01-20 09:09:58 +0100

    utils

---
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Node/Exec.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NodeJS.ChildProcessModule;
+
+
+namespace MasDev.Common.Saltarelle
+{
+	public static class Exec
+	{
+		public static Task<string> Command (string command)
+		{
+			var tcs = new TaskCompletionSource<string> ();
+			ChildProcess.Exec (command, (error, stdout, stderr) => tcs.SetResult (stdout.ToString ()));
+			return tcs.Task;
+		}
+	}
+}
+
---
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Typings/Printer/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NodePrinter
+{
+	[Imported, ModuleName (Printer.ModuleName), ScriptName ("")]
+	public class Printer
+	{
+		public const string ModuleName = "printer";
+
+		public PrinterObject[] GetPrinters ()
+		{
+			return null;
+		}
+	}
+
+
+	public class PrinterObject
+	{
+		public string Name;
+		public bool IsDefault;
+		public string Status;
+		public dynamic Options;
+	}
+}
+
