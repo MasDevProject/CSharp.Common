3b71af532759a08716380968fd83c127efe6c8a8
sykom <bonacin@outlook.com>
2015-01-30 13:36:50 +0100

    async apis

--- MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Extensions/ExpressExtensions.cs
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Extensions/ExpressExtensions.cs
@@ -1,4 +1,5 @@
-﻿using NodeExpress;
+﻿using System;
+using NodeExpress;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,11 @@ namespace MasDev.Extensions
 {
 	public static class ExpressExtensions
 	{
+        const int METHOD_GET = 0;
+        const int METHOD_POST = 1;
+        const int METHOD_PUT = 2;
+        const int METHOD_DELETE = 3;
+
 		public static void EnableCors (this ExpressApplication app, string host, IEnumerable<string> acceptedHeaders)
 		{
 			if (acceptedHeaders == null)
@@ -37,6 +43,62 @@ namespace MasDev.Extensions
 			server.Content = app.Listen (port, () => tcs.SetResult (server.Content));
 			return tcs.Task;
 		}
+
+        private static ExpressHandler CreateExpressHandler(TaskCompletionSource<object> tcs, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+	    {
+            return delegate(ExpressServerRequest request, ExpressServerResponse response)
+            {
+                var task = function(request, response);
+                task.GetAwaiter().OnCompleted(() =>
+                {
+                    if (task.Exception == null) tcs.SetResult(null);
+                    else tcs.SetException(task.Exception);
+                });
+            };
+	    }
+
+        private static async Task HandleAsync(this ExpressApplication app, string path, int method, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            var handler = CreateExpressHandler(tcs, function);
+            switch (method)
+            {
+                case METHOD_GET:
+                    app.Get(path, handler);
+                    break;
+                case METHOD_POST:
+                    app.Post(path, handler);
+                    break;
+                case METHOD_DELETE:
+                    app.Delete(path, handler);
+                    break;
+                case METHOD_PUT:
+                    app.Put(path, handler);
+                    break;
+                default:throw new ArgumentException("Method not mapped");
+            }
+            await tcs.Task;
+        }
+
+        public static async void GetAsync(this ExpressApplication app, string path, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+        {
+            await app.HandleAsync(path, METHOD_GET, function);
+        }
+
+        public static async void PostAsync(this ExpressApplication app, string path, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+        {
+            await app.HandleAsync(path, METHOD_POST, function);
+        }
+
+        public static async void DeleteAsync(this ExpressApplication app, string path, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+        {
+            await app.HandleAsync(path, METHOD_DELETE, function);
+        }
+
+        public static async void PutAsync(this ExpressApplication app, string path, Func<ExpressServerRequest, ExpressServerResponse, Task> function)
+        {
+            await app.HandleAsync(path, METHOD_PUT, function);
+        }
 	}
 
 	class WrappedObject
--- MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Node/Exec.cs
+++ MasDev.Common/Core/MasDev.Common.Core.Saltarelle/Source/Node/Exec.cs
@@ -6,7 +6,7 @@ namespace MasDev.Common.Saltarelle
 {
 	public static class Exec
 	{
-		public static Task<string> Command (string command)
+		public static Task<string> ExecAsync (string command)
 		{
 			var tcs = new TaskCompletionSource<string> ();
 			ChildProcess.Exec (command, (error, stdout, stderr) => tcs.SetResult (stdout.ToString ()));
