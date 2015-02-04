81dcedfb4ce61d0dec8d15c75b4371676cce86bf
francesco <francesco@francescos-MacBook-Pro.local>
2015-02-04 13:41:27 +0100

    MasDev.IOS FileIO and Logger implementation

---
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/Utils/Logger.cs
@@ -0,0 +1,18 @@
+﻿using MasDev.Utils;
+
+namespace MasDev.Common
+{
+	public class Logger : ILogger
+	{
+		public void Log (string tag, object message)
+		{
+			//TODO
+		}
+
+		public void Log (object message)
+		{
+			//TODO
+		}
+	}
+}
+
--- MasDev.Common/Core/MasDev.Common.Core.iOS/Source/iOSDependencyConfigurator.cs
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/iOSDependencyConfigurator.cs
@@ -2,6 +2,9 @@ using SQLite.Net.Interop;
 using SQLite.Net.Platform.XamarinIOS;
 using MasDev.Patterns.Injection;
 using MasDev.Utils;
+using MasDev.Common;
+using MasDev.IO;
+using MasDev.Security;
 
 namespace MasDev.iOS
 {
@@ -10,6 +13,10 @@ namespace MasDev.iOS
 		public void ConfigureDependencies (IDependencyContainer container)
 		{
 			container.AddDependency<ISQLitePlatform, SQLitePlatformIOS> (LifeStyles.Singleton);
+			container.AddDependency<ISymmetricCrypto, Aes> (LifeStyles.Singleton);
+			container.AddDependency<IFileIO, FileIO> (LifeStyles.Singleton);
+			container.AddDependency<IRegistry, Registry> (LifeStyles.Singleton);
+			container.AddDependency<ILogger, Logger> (LifeStyles.Singleton);
 			container.AddDependency<ITimeZoneConverter, TimeZoneConverter> (LifeStyles.Singleton);
 		}
 	}
