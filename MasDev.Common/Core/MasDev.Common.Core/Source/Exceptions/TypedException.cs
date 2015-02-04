d87a5b67ea0dc2d83e97ada788a1aff024debf43
francesco <francesco@francescos-MacBook-Pro.local>
2015-02-04 19:49:56 +0100

    Added TypedException

---
+++ MasDev.Common/Core/MasDev.Common.Core/Source/Exceptions/TypedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace MasDev.Exceptions
+{
+	public class TypedException<T> : Exception 
+	{
+		public T Cause { get; private set; }
+
+		internal TypedException (T exceptionEnum) : base (exceptionEnum.ToString ())
+		{
+			Cause = exceptionEnum;
+		}
+	}
+
+	public static class TypedException
+	{
+		public static TypedException<T> Create<T> (T exceptionEnum)
+		{
+			return new TypedException<T> (exceptionEnum);
+		}
+	}
+}
+
