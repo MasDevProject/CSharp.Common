d575306b09e5f7916fefbb80355016c74b762d32
Salvatore Di Stefano <Salvatore@MacBookalvatore.lan>
2015-02-08 13:07:21 +0100

    iOS Datetime/NSDate Extensions

---
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/Extensions/DateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Foundation;
+
+namespace MasDev.iOS.Extensions
+{
+	public static class DateExtensions
+	{
+		private static DateTime BaseDate = new DateTime(2001,1,1,0,0,0);
+
+		public static NSDate ToNSDate(this DateTime dateTime)
+		{
+			return NSDate.FromTimeIntervalSinceReferenceDate((dateTime-(BaseDate)).TotalSeconds);
+		}
+
+		public static DateTime ToDateTime(this NSDate nsDate)
+		{
+			return (BaseDate).AddSeconds(nsDate.SecondsSinceReferenceDate);
+		}
+	}
+}
