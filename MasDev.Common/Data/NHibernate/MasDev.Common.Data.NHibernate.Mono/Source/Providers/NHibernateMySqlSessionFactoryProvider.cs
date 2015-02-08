7519cb3fb7de9adc2eeb2d4ca502eaf8c8cc8af5
Gianluca Bonacin <gianlucabonacin@Gianlucas-MacBook-Pro.local>
2015-02-08 21:50:37 +0100

    fixed ignored parameter

--- MasDev.Common/Data/NHibernate/MasDev.Common.Data.NHibernate.Mono/Source/Providers/NHibernateMySqlSessionFactoryProvider.cs
+++ MasDev.Common/Data/NHibernate/MasDev.Common.Data.NHibernate.Mono/Source/Providers/NHibernateMySqlSessionFactoryProvider.cs
@@ -13,7 +13,7 @@ namespace MasDev.Data.NHibernate.Providers
 		{
 			FactoryLazy = new Lazy<ISessionFactory> (() => {
 				var model = NHibernateUtils.CreateMappings<TMappingProvider> (modelsNamespace);
-				return NHibernateUtils.BuildMySqlSessionFactory<TMappingProvider> (host, database, username, password, model, context);
+				return NHibernateUtils.BuildMySqlSessionFactory<TMappingProvider> (host, database, username, password, model, context, buildSchema);
 			});
 		}
 
