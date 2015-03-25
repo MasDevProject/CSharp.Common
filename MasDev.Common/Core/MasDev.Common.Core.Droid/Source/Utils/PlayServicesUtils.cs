34e59a45461962fe793089522ce7f9ad12e94423
francesco <francesco@francescos-MacBook-Pro.local>
2015-03-25 19:32:07 +0100

    PlayServicesutils

---
+++ MasDev.Common/Core/MasDev.Common.Core.Droid/Source/Utils/PlayServicesUtils.cs
@@ -0,0 +1,32 @@
+﻿using Android.Gms.Common;
+using Android.App;
+using MasDev.Patterns.Injection;
+using MasDev.Utils;
+
+
+namespace MasDev.Common
+{
+	public static class PlayServicesUtils
+	{
+		public static int PLAY_SERVICES_RESOLUTION_REQUEST = 1234567;
+
+		public static bool CheckPlayServices (Activity activity) 
+		{
+			int resultCode = GooglePlayServicesUtil.IsGooglePlayServicesAvailable(activity);
+			if (resultCode != ConnectionResult.Success) 
+			{
+				if (GooglePlayServicesUtil.IsUserRecoverableError (resultCode)) {
+					GooglePlayServicesUtil.GetErrorDialog(resultCode, activity, PLAY_SERVICES_RESOLUTION_REQUEST).Show();
+				} else {
+					var logger = Injector.Resolve<ILogger> ();
+					if (logger != null)
+						logger.Log ("Device not supported");
+					activity.Finish ();
+				}
+				return false;
+			}
+			return true;
+		}
+	}
+}
+
