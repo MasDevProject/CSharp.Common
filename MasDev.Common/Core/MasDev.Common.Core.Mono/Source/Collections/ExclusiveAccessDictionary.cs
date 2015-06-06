3990e917d3df45a1280ee8f829414bb209771702
sykom <bonacin@outlook.com>
2015-06-06 18:29:16 +0200

    no message

--- MasDev.Common/Core/MasDev.Common.Core.Mono/Source/Collections/ExclusiveAccessDictionary.cs
+++ MasDev.Common/Core/MasDev.Common.Core.Mono/Source/Collections/ExclusiveAccessDictionary.cs
@@ -4,35 +4,46 @@ using MasDev.Utils;
 
 namespace MasDev.Common
 {
-	public abstract class ExclusiveAccessDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IExclusiveAccessProvider
-	{
-		readonly object _lock = new object ();
+    public abstract class ExclusiveAccessDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IExclusiveAccessProvider
+    {
+        readonly object _lock = new object();
 
-		public virtual IDisposable StartExclusiveAccess (IIdentifier identifier)
-		{
-			lock (_lock) {
-				return new LockByIdMutex (identifier.Identify);
-			}
-		}
+        public virtual IDisposable StartExclusiveAccess(IIdentifier identifier)
+        {
+            lock (_lock)
+            {
+                //return new LockByIdMutex (identifier.Identify); TODO scoprire perchè esplode
+                return new DummyDisposable();
+            }
+        }
 
-		public abstract IIdentifier GetIdentifier (TKey key);
-	}
+        public abstract IIdentifier GetIdentifier(TKey key);
 
-	class Identifier<TKey, TValue> : IIdentifier
-	{
-		const string IdentifierFormat = "{0}_{1}_{2}s";
-		readonly string _keyTypeName = typeof(TKey).Name;
-		readonly string _valueTypeName = typeof(TValue).Name;
-		readonly TKey _keyInstance;
+        private class DummyDisposable : IDisposable
+        {
+            public void Dispose()
+            {
 
-		public Identifier (TKey keyInstance)
-		{
-			_keyInstance = keyInstance;
-		}
+            }
+        }
+    }
 
-		public string Identify {
-			get { return string.Format (IdentifierFormat, _keyTypeName, _keyInstance, _valueTypeName); }
-		}
-	}
+    class Identifier<TKey, TValue> : IIdentifier
+    {
+        const string IdentifierFormat = "{0}_{1}_{2}s";
+        readonly string _keyTypeName = typeof(TKey).Name;
+        readonly string _valueTypeName = typeof(TValue).Name;
+        readonly TKey _keyInstance;
+
+        public Identifier(TKey keyInstance)
+        {
+            _keyInstance = keyInstance;
+        }
+
+        public string Identify
+        {
+            get { return string.Format(IdentifierFormat, _keyTypeName, _keyInstance, _valueTypeName); }
+        }
+    }
 }
 
