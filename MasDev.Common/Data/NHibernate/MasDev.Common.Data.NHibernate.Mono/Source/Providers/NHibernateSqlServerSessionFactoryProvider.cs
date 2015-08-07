e438420b48f58abdab6d749d78d050e54e614fb5
Gianluca Bonacin <gianluca.bonacin@gmail.com>
2015-08-07 16:42:52 +0200

    SqlServer provider

--- MasDev.Common/Data/NHibernate/MasDev.Common.Data.NHibernate.Mono/Source/Providers/NHibernateSqlServerSessionFactoryProvider.cs
+++ MasDev.Common/Data/NHibernate/MasDev.Common.Data.NHibernate.Mono/Source/Providers/NHibernateSqlServerSessionFactoryProvider.cs
@@ -7,24 +7,35 @@ namespace MasDev.Data.NHibernate.Providers
 	public class NHibernateSqlServerSessionFactoryProvider<TMappingProvider> : ISessionFactoryProvider 
         where TMappingProvider : PersistenceMapper, new()
 	{
-		protected readonly Lazy<ISessionFactory> FactoryLazy;
+        ISessionFactory _factory;
+        readonly string _modelsNamespace;
+        readonly string _host;
+        readonly string _database;
+        readonly string _schema;
+        readonly string _username;
+        readonly string _password;
+        readonly string _context;
+        readonly bool _buildSchema;
 
-		public NHibernateSqlServerSessionFactoryProvider (string modelsNamespace, string host, string database, string schema, string username, string password, string context, bool buildSchema = false)
+        public NHibernateSqlServerSessionFactoryProvider (string modelsNamespace, string host, string database, string schema, string username, string password, string context, bool buildSchema = false)
 		{
-			throw new NotImplementedException ();
-			FactoryLazy = new Lazy<ISessionFactory> (() => {
-				var model = NHibernateUtils.CreateMappings<TMappingProvider> (modelsNamespace);
-				var factory = NHibernateUtils.BuildSqlServerFactory<TMappingProvider> (host, database, schema, username, password, model, context, buildSchema);
-				return factory;
-			});
+            _modelsNamespace = modelsNamespace;
+            _host = host;
+            _database = database;
+            _schema = schema;
+            _username = username;
+            _password = password;
+            _context = context;
+            _buildSchema = buildSchema;
 		}
 
-		public virtual ISessionFactory Factory { get { return FactoryLazy.Value; } }
+		public virtual ISessionFactory Factory { get { return _factory; } }
 
 		public void Connect ()
 		{
-			throw new NotImplementedException ();
-		}
+            var model = NHibernateUtils.CreateMappings<TMappingProvider>(_modelsNamespace);
+            var factory = NHibernateUtils.BuildSqlServerFactory<TMappingProvider>(_host, _database, _schema, _username, _password, model, _context, _buildSchema);
+        }
 	}
 }
 
