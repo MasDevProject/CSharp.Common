47ba6f0884380d3a212034a551c4174e0f9acf07
Francesco <mazzarol.francesco@gmail.com>
2015-08-10 14:04:29 +0200

    html body email

--- MasDev.Common/Core/MasDev.Common.Core.Mono/Source/Mailing/MailClient.cs
+++ MasDev.Common/Core/MasDev.Common.Core.Mono/Source/Mailing/MailClient.cs
@@ -5,32 +5,21 @@ namespace MasDev.Mono
 {
 	public static class MailClient
 	{
-		public static void SendViaGmail (string to, string subject, string body, string username, string password, string senderEmail, string senderAlias)
+		public static void SendViaGmail (string to, string subject, string body, string username, string password, string senderEmail, string senderAlias, bool htmlBody)
 		{
-			var mail = new MailMessage ();
-			mail.From = new MailAddress (senderEmail, senderAlias);
-			mail.To.Add (to);
-			mail.Subject = subject;
-			mail.Body = body;
-
-			var client = new SmtpClient ("smtp.gmail.com");
-			client.Port = 587;
-			client.Credentials = new NetworkCredential (username, password);
-			client.EnableSsl = true;
-
-			ServicePointManager.ServerCertificateValidationCallback = delegate {
-				return true;
-			};
-			client.Send (mail);
+			Send ("smtp.gmail.com", 587, subject, body, username, password, senderEmail, senderAlias, htmlBody, to);
 		}
 
-		public static void Send (string smtp, int port, string to, string subject, string body, string username, string password, string senderEmail, string senderAlias)
+		public static void Send (string smtp, int port, string subject, string body, string username, string password, string senderEmail, string senderAlias, bool htmlBody, params string[] to)
 		{
 			var mail = new MailMessage ();
 			mail.From = new MailAddress (senderEmail, senderAlias);
-			mail.To.Add (to);
 			mail.Subject = subject;
 			mail.Body = body;
+			mail.IsBodyHtml = htmlBody;
+
+			foreach (var email in to)
+				mail.To.Add (email);
 
 			var client = new SmtpClient (smtp);
 			client.Port = port;
