4aec6e4f56c43d1b062391063ab1ed4cc790178a
Francesco <francesco@Francescos-MacBook-Pro.local>
2015-10-13 13:10:52 +0200

    fixed GetScaledBitmap

--- MasDev.Common/Core/MasDev.Common.Core.Droid/Source/Utils/BitmapUtils.cs
+++ MasDev.Common/Core/MasDev.Common.Core.Droid/Source/Utils/BitmapUtils.cs
@@ -9,6 +9,43 @@ namespace MasDev.Droid.Utils
 {
 	public static class BitmapUtils
 	{
+		static async Task<Bitmap> GetScaledBitmap (string path, int width, int height)
+		{
+			return await Task.Run(() => {
+				var options = new BitmapFactory.Options();
+				options.InJustDecodeBounds = true;
+				BitmapFactory.DecodeFile(path, options);
+
+				options.InSampleSize =  CalculateInSampleSize (options, width, height);
+
+				options.InJustDecodeBounds = false;
+				return BitmapFactory.DecodeFile(path, options);
+			});
+		}
+
+		static int CalculateInSampleSize (BitmapFactory.Options options, int reqWidth, int reqHeight) 
+		{
+			// Raw height and width of image
+			var height = options.OutHeight;
+			var width = options.OutWidth;
+			var inSampleSize = 1;
+
+			if (height > reqHeight || width > reqWidth) {
+
+				var halfHeight = height / 2;
+				var halfWidth = width / 2;
+
+				// Calculate the largest inSampleSize value that is a power of 2 and keeps both
+				// height and width larger than the requested height and width.
+				while ((halfHeight / inSampleSize) > reqHeight
+					&& (halfWidth / inSampleSize) > reqWidth) {
+					inSampleSize *= 2;
+				}
+			}
+
+			return inSampleSize;
+		}
+
 		public static async Task<MimedStream> ScaleToStreamAsync(string filePath, int maxSize)
 		{
 			var fi = new FileInfo(filePath);
@@ -26,22 +63,6 @@ namespace MasDev.Droid.Utils
 				var resizedBitmap = Bitmap.CreateScaledBitmap (source, (int)(source.Width * scale), (int)(source.Height * scale), true);
 				return new MimedStream (await resizedBitmap.AsStreamAsync (Bitmap.CompressFormat.Png), Mime.FromPath (filePath), fileName);
 			}
-		}
-
-		public static async Task<Bitmap> GetScaledBitmap(string imagePath, int requiredSize)
-		{
-			var o = new BitmapFactory.Options ();
-			o.InJustDecodeBounds = true;
-			await BitmapFactory.DecodeStreamAsync (new FileStream(imagePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite), null, o);
-
-			int scale=1;
-			while(o.OutWidth/scale/2 >= requiredSize && o.OutHeight/scale/2 >= requiredSize)
-				scale*=2;
-
-			//Decode with inSampleSize
-			var o2 = new BitmapFactory.Options();
-			o2.InSampleSize = scale;
-			return await BitmapFactory.DecodeStreamAsync(new FileStream(imagePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite), null, o2);
 		}
 	}
 }
