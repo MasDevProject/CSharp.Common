39545aad767ad35de361169094720e628b2d2e46
Francesco <francesco@Francescos-MacBook-Pro.local>
2015-11-04 21:33:05 +0100

    tmp

---
+++ MasDev.Common/Core/MasDev.Common.Core.Droid/Source/Utils/ViewHolderWithPositionListener.cs
@@ -0,0 +1,28 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using System;
+
+namespace MasDev.Droid.Utils
+{
+	public class ViewHolderWithPositionListener : RecyclerView.ViewHolder, View.IOnClickListener
+	{
+		public event Action<int> OnItemClick;
+
+		public ViewHolderWithPositionListener (View rootView) : base (rootView)
+		{
+			rootView.SetOnClickListener (this);
+		}
+
+		public ViewHolderWithPositionListener (View rootView, Action<int> onItemClick) : this (rootView)
+		{
+			OnItemClick = onItemClick;
+		}
+
+		public void OnClick (View v)
+		{
+			if (OnItemClick != null)
+				OnItemClick (LayoutPosition);
+		}
+	}
+}
+
