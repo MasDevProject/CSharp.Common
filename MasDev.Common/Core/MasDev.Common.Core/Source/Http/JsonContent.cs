f328c9ff42b9636692b0113bcaaa8c6bfae937d3
Francesco <francesco@Francescos-MBP.homenet.telecomitalia.it>
2015-12-30 14:45:29 +0100

    JsonContent

---
+++ MasDev.Common/Core/MasDev.Common.Core/Source/Http/JsonContent.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MasDev.IO.Http
+{
+	public class JsonContent<T> : StringContent
+	{
+		const string _contentType = "application/json";
+		public readonly T Content;
+
+		public JsonContent (T obj) : base (JsonConvert.SerializeObject (obj), Encoding.UTF8, _contentType)
+		{
+			Content = obj;
+		}
+	}
+}
+
