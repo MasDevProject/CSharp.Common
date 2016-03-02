84878ff1a09e48d67cd1223e7e8aa4590272139d
SDiStefano <salvatore.distefano@factorymind.com>
2016-03-02 15:11:11 +0100

    - NSUrlSession download

---
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/App/Delegates/BaseSessionDownloadManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Foundation;
+using System.IO;
+
+namespace MasDev.Common
+{
+	public interface IDownloadDelegateConfigurator
+	{
+		Action GetBackgroundSessionCompletionHandler();
+
+		void HandleDownloadsFinished();
+	}
+
+	public class BaseSessionDownloadDelegate : NSUrlSessionDownloadDelegate
+	{
+		readonly IDownloadDelegateConfigurator _configurator;
+		Action<string, string> _onFileDownloadComplete;
+
+		public BaseSessionDownloadDelegate(IDownloadDelegateConfigurator configurator, Action<string, string> onFileDownloadComplete)
+		{
+			_configurator = configurator;
+			_onFileDownloadComplete = onFileDownloadComplete;
+		}
+
+		public override void DidFinishDownloading (NSUrlSession session, NSUrlSessionDownloadTask downloadTask, NSUrl location)
+		{
+			#if DEBUG
+
+			Console.WriteLine ("File downloaded in : {0}", location);
+
+			#endif
+
+			var fileManager = NSFileManager.DefaultManager;
+			var downloadUrl = downloadTask.Response.Url.ToString ();
+			var destinationPath = downloadTask.TaskDescription;
+
+			NSError errorCopy, errorRemove;
+			var fileName = Path.GetFileName (destinationPath);
+			var fileFolder = destinationPath.Replace(fileName, string.Empty);
+
+			fileManager.Remove (destinationPath, out errorRemove);
+
+			Directory.CreateDirectory(fileFolder);
+
+			fileManager.Copy (location.Path, destinationPath, out errorCopy);
+
+			#if DEBUG
+
+			if(errorCopy != null)
+				Console.WriteLine (errorCopy.LocalizedDescription);
+
+			#endif
+
+			if (_onFileDownloadComplete != null)
+				_onFileDownloadComplete.Invoke (downloadUrl, destinationPath);
+		}
+
+		public override void DidFinishEventsForBackgroundSession (NSUrlSession session)
+		{
+			var handler = _configurator.GetBackgroundSessionCompletionHandler ();
+
+			// call completion handler when you're done
+			if (handler != null) 
+			{
+				_configurator.HandleDownloadsFinished ();
+
+				handler.Invoke ();
+			}
+		}
+
+		public override void DidWriteData (NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long bytesWritten, long totalBytesWritten, long totalBytesExpectedToWrite)
+		{
+			#if DEBUG
+			Console.WriteLine("["+ downloadTask.TaskIdentifier +"] download: " + Math.Round(((float) totalBytesWritten / totalBytesExpectedToWrite) * 100, 2) + "%");
+			#endif
+		}
+	}
+}
---
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/Utils/BaseDownloadService.cs
@@ -0,0 +1,99 @@
+﻿using Foundation;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MasDev.Common
+{
+	public abstract class BaseDownloadService : IDownloadDelegateConfigurator
+	{
+		NSUrlSession _session;
+		NSUrlSessionDelegate _sessionDelegate;
+
+		protected NSUrlSession Session
+		{
+			get
+			{
+				if (_session == null)
+					_session = InitializeSession ();
+
+				return _session;
+			}
+		}
+
+		protected virtual NSUrlSession InitializeSession()
+		{
+			NSUrlSession session = null;
+
+			using (var configuration = NSUrlSessionConfiguration.CreateBackgroundSessionConfiguration (SessionIdentifier))
+			{
+				ConfigureSession (configuration);
+
+				_sessionDelegate = CreateSessionDelegate();
+
+				session = NSUrlSession.FromConfiguration (configuration, _sessionDelegate, null);
+			}
+
+			return session;
+		}
+
+		// Abstract members
+
+		protected abstract string SessionIdentifier { get; }
+
+		protected abstract void ConfigureSession(NSUrlSessionConfiguration configuration);
+
+		public abstract Action GetBackgroundSessionCompletionHandler ();
+
+		public abstract void HandleDownloadCompleted (string url, string destinationPath);
+
+		public abstract void HandleDownloadsFinished ();
+
+		protected virtual NSUrlSessionDelegate CreateSessionDelegate()
+		{
+			return new BaseSessionDownloadDelegate (this, HandleDownloadCompleted);
+		}
+
+		// Public methods
+
+		public void ResetSession()
+		{
+			if (_session == null)
+				return;
+
+			_sessionDelegate = null;
+
+			_session.InvalidateAndCancel();
+			_session = null;
+		}
+
+		public async Task<IEnumerable<NSUrlSessionTask>> GetPendingTasks()
+		{
+			var tasks = await Session.GetTasks2Async ();
+
+			if (tasks != null && tasks.DownloadTasks != null)
+				return (tasks.DownloadTasks).ToList ();
+
+			return null;
+		}
+
+		public void EnqueueTask(string url, string destinationPath)
+		{
+			var task = CreateTask (url);
+
+			task.TaskDescription = destinationPath;
+			task.Resume ();
+		}
+
+		// Utils
+
+		protected virtual NSUrlSessionDownloadTask CreateTask(string url)
+		{
+			var downloadURL = NSUrl.FromString (new NSString(url));
+			var request = NSUrlRequest.FromUrl (downloadURL);
+
+			return Session.CreateDownloadTask (request);
+		}
+	}
+}
