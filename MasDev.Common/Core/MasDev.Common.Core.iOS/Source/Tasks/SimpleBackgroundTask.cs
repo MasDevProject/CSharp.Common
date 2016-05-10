8b31dd3bb7c3cf37ec18a05720105d473dfab3b0
SDiStefano <salvatore.distefano@factorymind.com>
2016-05-10 15:50:05 +0200

    - BackgroundTask util

---
+++ MasDev.Common/Core/MasDev.Common.Core.iOS/Source/Tasks/SimpleBackgroundTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace MasDev.Common
+{
+	public static class SimpleBackgroundTask
+	{
+		const int EmptyTaskId = -1;
+
+		static nint taskId = EmptyTaskId;
+
+		public static async void StartTask (Func<Task> taskFunction)
+		{
+			taskId = UIApplication.SharedApplication.BeginBackgroundTask (OnExpire);
+
+			try
+			{
+				await Task.Run (async () => await taskFunction.Invoke());
+			}
+			finally 
+			{
+				if (taskId != EmptyTaskId)
+					UIApplication.SharedApplication.EndBackgroundTask (taskId);
+
+				taskId = EmptyTaskId;
+			}
+		}
+
+		static void OnExpire()
+		{
+			if(taskId != EmptyTaskId)
+				UIApplication.SharedApplication.EndBackgroundTask (taskId);
+
+			taskId = EmptyTaskId;
+		}
+	}
+}
